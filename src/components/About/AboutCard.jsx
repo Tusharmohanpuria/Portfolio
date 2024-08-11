6af024d1983aa49886540dbb95706220f2ede0c7
Tusharmohanpuria <tusharmohanpuria@iiitmanipur.ac.in>
2024-08-11 23:14:39 +0530

    Updated

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -4,37 +4,36 @@ import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
-    <Card className="quote-card-view">
+    <Card className="about-card">
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Tushar Mohanpuria </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hello! I'm <span className="purple">Tushar Mohanpuria</span>, 
+            currently pursuing a B.Tech in Computer Science Engineering at the Indian Institute of Information Technology, Manipur.
             <br />
-            I am currently employed as a software developer at Juspay.
+            I have a strong interest in software development and cybersecurity, with hands-on experience in ethical hacking and penetration testing.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am proficient in various programming languages, including C++, Python, and JavaScript, and have developed several full-stack projects using the MERN stack.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            When I'm not coding, you can find me:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Gaming
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading Novels
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Watching Animation & Movies
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Aim to create solutions that make a meaningful impact!"
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Tushar</footer>
         </blockquote>
       </Card.Body>
     </Card>
