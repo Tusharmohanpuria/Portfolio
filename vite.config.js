4f4ab27536388bb6c17e950131f0031c3966a872
Tusharmohanpuria <tusharmohanpuria@iiitmanipur.ac.in>
2024-08-12 02:18:39 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,16 @@ export default defineConfig({
   server: {
     open: true,
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
